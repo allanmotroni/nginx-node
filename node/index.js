77e5a0223b408da94509dc7ed7dfae1918f04b5b
Allan Motroni <allan.motroni@paygo-consult.com.br>
2022-08-28 13:41:26 -0300

    fix: ajuste na aplicação, dockerfile e docker-compose

--- node/index.js
+++ node/index.js
@@ -14,24 +14,39 @@ const mysql = require('mysql')
 app.get('/', (req, res) => {
 
     const connection = mysql.createConnection(config)
+    try {
+        var name = 'ALLAN'
+        const sqlInsert = `INSERT INTO people (name) VALUES ('${name}')`
 
-    var name = 'ALLAN'
-    const sqlInsert = `INSERT INTO people (name) VALUES ('${name}')`
-    connection.query(sqlInsert)
+        connection.query(sqlInsert)
 
-    console.log(`Nome: '${name}' inserido no banco de dados!`)
+        console.log(`Nome: '${name}' inserido no banco de dados!`)
+    } catch (error) {
+        console.error(`ERRO NO INSERT: ${error}`)
+    }
 
     const sqlGet = `SELECT name FROM people`
 
     let retorno = '<h1>Full Cycle Rocks!</h1>'
     connection.query(sqlGet, function (err, result, fields) {
-        retorno += '<ol>'
-        for (let index = 0; index < result.length; index++) {
-            const element = result[index];
-            console.log(element.name)
-            retorno += `<li>${element.name}</li>`
+        if (err) {
+            console.error(`ERRO NO SELECT: ${err}`)
         }
-        retorno += '</ol>'
+
+        if (result) {
+            console.log('RESULTADO!')
+            retorno += '<ol>'
+            for (let index = 0; index < result.length; index++) {
+                const element = result[index];
+                console.log(element.name)
+                retorno += `<li>${element.name}</li>`
+            }
+            retorno += '</ol>'
+        }
+        else {
+            console.log('SEM RESULTADO')
+            retorno = 'SEM RESULTADO!'
+        }
 
         res.send(retorno)
     })
